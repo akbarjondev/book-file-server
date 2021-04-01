0de42cb17f75e927a9ccdb0157402f38c573077a
akbarjondev <tojiyev.akbarjon@gmail.com>
2021-04-01 12:46:49 +0500

    Set secret key to the environment

--- resources/jwt/jwt.js
+++ resources/jwt/jwt.js
@@ -1,5 +1,6 @@
 const jwt = require('jsonwebtoken')
+require('dotenv').config()
 
 module.exports = {
-	verify: (token) => jwt.verify(token, '_edo')
+	verify: (token) => jwt.verify(token, process.env.secret_key)
 }
