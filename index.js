36aa896a4dd035d7f8ff5dc3568b2cc28835b8b3
akbarjondev <tojiyev.akbarjon@gmail.com>
2021-04-01 13:01:48 +0500

    Add storage to the multer

--- index.js
+++ index.js
@@ -1,9 +1,27 @@
 const express = require('express')
+const multer = require('multer')
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, '/books')
+  },
+  filename: function (req, file, cb) {
+    const fileName = file.fieldname + '-' + Date.now()
+
+    cb(null, fileName)
+  }
+})
+ 
+const upload = multer({ storage: storage })
 
 const app = express()
 
 const PORT = process.env.PORT || 3000
 
 app.use(express.json())
+
+app.post('/upload', upload.array('books', 12), (req, res, next) => {
+
+})
 
 app.listen(PORT, () => console.log(`ready at http://localhost:${PORT}`))
